0fff5ed3534fec0f48a78dac592fcdc2bbb8cbe9
ksilvafe <ferreirakevin325@gmail.com>
2025-12-13 19:56:25 -0300

    feat: created component header links 🔨🔨🔨🔨

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+import { Header } from "@/components/header/header";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
+    <Header />
   );
 }
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,28 @@
+import Link from "next/link"
+
+interface NavHeadersLink {
+  href: string
+  label: string
+}
+const navHeaders: NavHeadersLink[] = [
+  { href: "/projects", label: "/projects" },
+  { href: "/articles", label: "/articles" },
+  { href: "/bookmarks", label: "/bookmarks" },
+]
 export function Header() {
   return (
-    <div>Header</div>
+    <div className="max-w-150 flex flex-col md:flex-row items-center md:items-center mx-auto gap-8 pt-8 md:justify-between">
+      <div>
+        <div>Kevin Ferreira</div>
+        <div>Engenheiro de Software</div>
+      </div>
+      <div className="flex justify-between items-center md:ml-auto gap-4">
+        <button>🌙</button>
+        <button>🇧🇷</button>
+        {navHeaders.map((link) => (
+          <Link key={link.href} href={link.href}>{link.label}</Link>
+        ))}
+      </div>
+    </div>
   )
 }
