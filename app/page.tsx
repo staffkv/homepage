eb851f57c2d1d1f4302cbe1dcd39ec8c2794dfbc
ksilvafe <ferreirakevin325@gmail.com>
2025-12-15 01:55:18 -0300

    fix: remove imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import { ContentLinks } from "@/components/contentLinks";
-import { Header } from "@/components/header/header";
-import { Card, CardContent } from "@/components/ui/card";
 
 export default function Home() {
   return (
