381fb49af2c6cf85f83aa3de3dddade4c2294f0a
ksilvafe <ferreirakevin325@gmail.com>
2025-12-24 00:57:50 -0300

    fix: add path return home link header

--- components/header.tsx
+++ components/header.tsx
@@ -10,8 +10,10 @@ export function Header() {
   return (
     <div className="max-w-150 flex flex-col md:flex-row items-center md:items-center mx-auto gap-8 pt-8 md:justify-between">
       <div className="text-center md:text-left">
-        <div className="font-semibold text-(--text-color-headings)">{t.name}</div>
-        <div className="font-medium text-(--text-color-secondary)">{t.title}</div>
+        <Link href="/" className="hover:underline">
+         <div className="font-semibold text-(--text-color-headings)">{t.name}</div>
+         <div className="font-medium text-(--text-color-secondary)">{t.title}</div>
+        </Link>
       </div>
       <div className="flex justify-between items-center md:ml-auto gap-2.5 text-(--text-color-secondary)">
         <ThemeToggle />
